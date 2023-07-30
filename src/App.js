d35597295c9f3490530440c6a4f61c9d1d44c6f1
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-07-31 00:30:16 +0300

    Updated README file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import BotContext from './BotContext';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"  style={{
+      overflowY: 'auto', // Add vertical scroll if the content overflows
+      maxHeight: '100vh', // Set the maximum height of the container
+  }}>
      
       <BotContext />
     </div>
